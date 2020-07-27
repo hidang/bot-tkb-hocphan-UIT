c753349e20565c96fd47aab51a9e096b77749ee2
hidang <yellowstudio.vn@gmail.com>
2020-07-27 16:17:09 +0700

    okokoko

--- index.js
+++ index.js
@@ -49,45 +49,51 @@ app.get('/webhook', (req, res) => {
 
 app.post("/webhook", (req, res) => {
   let body = req.body;
-
   // Checks this is an event from a page subscription
   if (body.object === 'page') {
-
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
-
       // Gets the message. entry.messaging is an array, but 
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
 
       console.log(webhook_event);
-      //sendMessage(body.id, webhook_event);
+      if (webhook_event.message) {
+        // Nếu người dùng gửi tin nhắn đến
+        if (webhook_event.message.text) {
+          var text = webhook_event.message.text;
+          if(text == 'hi' || text == "hello")
+          {
+            sendMessage(senderId, "Đỗ Văn Bot: " + 'Xin Chào');
+          }
+          else{sendMessage(senderId, "Đỗ Văn Bot: " + "Xin lỗi, câu hỏi của bạn chưa có trong hệ thống, chúng tôi sẽ cập nhật sớm nhất.");}
+        }
+      }
     });
 
     // Returns a '200 OK' response to all requests
     res.status(200).send('EVENT_RECEIVED');
-  } else {
+  }else{
     // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
-
 });
 
 // Đây là function dùng api của facebook để gửi tin nhắn
-// function sendMessage(senderId, message) {
-//   request({
-//     url: 'https://graph.facebook.com/v2.6/me/messages',
-//     qs: {
-//       access_token: PAGE_ACCESS_TOKEN,
-//     },
-//     method: 'POST',
-//     json: {
-//       recipient: {
-//         id: senderId
-//       },
-//       message: {
-//         text: message
-//       },
-//     }
-//   });
-// }
+function sendMessage(senderId, message) {
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      access_token: PAGE_ACCESS_TOKEN,
+    },
+    method: 'POST',
+    json: {
+      recipient: {
+        id: senderId
+      },
+      message: {
+        text: message
+      },
+    }
+  });
+}
