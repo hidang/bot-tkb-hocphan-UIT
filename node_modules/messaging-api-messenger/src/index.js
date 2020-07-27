de19230ea397363dba4b0b37af6702ef768cd65a
hidang <yellowstudio.vn@gmail.com>
2020-07-27 15:29:36 +0700

    okokoko

---
+++ node_modules/messaging-api-messenger/src/index.js
@@ -0,0 +1,14 @@
+/* @flow */
+
+import Messenger from './Messenger';
+import MessengerBatch from './MessengerBatch';
+import MessengerBroadcast from './MessengerBroadcast';
+import MessengerClient from './MessengerClient';
+
+export { Messenger, MessengerBatch, MessengerClient, MessengerBroadcast };
+export default {
+  Messenger,
+  MessengerBatch,
+  MessengerClient,
+  MessengerBroadcast,
+};
