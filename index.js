9ddbf5544b3bc62467724ad314f19fe417b844fe
hidang <yellowstudio.vn@gmail.com>
2020-07-27 20:06:21 +0700

    okokoko

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.post("/webhook", (req, res) => {
 // Gửi thông tin tới REST API để Bot tự trả lời
 function sendMessage(senderId, message) {
   request({
-    url: 'https://graph.facebook.com/v7.0/me/messages?',
+    url: 'https://graph.facebook.com/v7.0/me/messages',
     qs: {
       access_token: PAGE_ACCESS_TOKEN,
     },
