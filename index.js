04f826ddd5741619fbfce1a65c71e26b7f252321
hidang <yellowstudio.vn@gmail.com>
2020-07-28 21:35:13 +0700

    okokoko

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.get('/webhook', (req, res) => {
 
 app.post("/webhook", (req, res) => {
     let body = req.body;
-
+    console.log(body);
     // Checks this is an event from a page subscription
     if (body.object === 'page') {
   
