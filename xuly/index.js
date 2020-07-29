fc9c8fc808444a82ac98e21f0d28a7afe96fd744
hidang <yellowstudio.vn@gmail.com>
2020-07-29 22:37:36 +0700

    upne

--- xuly/index.js
+++ xuly/index.js
@@ -211,7 +211,7 @@ function addPersistentMenu(){
  function callSendAPI(messageData) {
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: PAGE_ACCESS_TOKEN },
+    qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
     method: 'POST',
     json: messageData
 
