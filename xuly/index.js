58cfec98652879543b691fff144361eefe5c2cf4
hidang <yellowstudio.vn@gmail.com>
2020-07-30 10:05:04 +0700

    upne

--- xuly/index.js
+++ xuly/index.js
@@ -232,30 +232,30 @@ function removePersistentMenu(){
  })
  }
 
- function callSendAPI(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
-    method: 'POST',
-    json: messageData
+//  function callSendAPI(messageData) {
+//   request({
+//     uri: 'https://graph.facebook.com/v2.6/me/messages',
+//     qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+//     method: 'POST',
+//     json: messageData
 
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
+//   }, function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//       var recipientId = body.recipient_id;
+//       var messageId = body.message_id;
 
-      if (messageId) {
-        console.log("Successfully sent message with id %s to recipient %s", 
-          messageId, recipientId);
-      } else {
-      console.log("Successfully called Send API for recipient %s", 
-        recipientId);
-      }
-    } else {
-      console.error("Unable to send message. :" + response.error);
-    }
-  });  
-}
+//       if (messageId) {
+//         console.log("Successfully sent message with id %s to recipient %s", 
+//           messageId, recipientId);
+//       } else {
+//       console.log("Successfully called Send API for recipient %s", 
+//         recipientId);
+//       }
+//     } else {
+//       console.error("Unable to send message. :" + response.error);
+//     }
+//   });  
+// }
 
 function sendTypingOn(recipientId) {
   console.log("Turning typing indicator on");
