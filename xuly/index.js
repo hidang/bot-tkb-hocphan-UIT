e43a87a5aecc4a497a2d7903b9bcc4f86e5c20bf
hidang <yellowstudio.vn@gmail.com>
2020-07-31 14:21:06 +0700

    n_g_u

--- xuly/index.js
+++ xuly/index.js
@@ -183,7 +183,7 @@ function callSendAPI(style, response) {
   //console.log('Sender PSID by callSendAPI: ' + sender_psid);
   // Send the HTTP request to the Messenger Platform
   request({
-    "uri": "https://graph.facebook.com/v7.0/me/" + style,
+    "uri": "https://graph.facebook.com/v2.6/me/" + style,
     "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
     "method": "POST",
     "json": response
