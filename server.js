2780ee9eee57abcf9a77d8cd2aaead965b709c6a
hidang <yellowstudio.vn@gmail.com>
2020-08-01 21:48:01 +0700

    byDang

--- server.js
+++ server.js
@@ -1,97 +1,95 @@
-'use strict';
+"use strict";
 ///////////////////////////////////////////////SETUP_SERVER//////////////////////////////////////////////////
-var xuly = require('./xuly/index.js');//xuly tin nhan
-require('dotenv').config();//Thư viện dùng .env -> dấu token pass...
+var xuly = require("./xuly/index.js"); //xuly tin nhan
+require("dotenv").config(); //Thư viện dùng .env -> dấu token pass...
 
 var express = require("express");
-var  bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 
 var app = express();
 app.use(bodyParser.json(), express.static("public")); // creates express http server
 app.set("view engine", "ejs");
 app.set("views", "./views");
 var server = require("http").Server(app);
-server.listen(process.env.PORT || 3000, () => console.log('Server is listening'));
+server.listen(process.env.PORT || 3000, () =>
+  console.log("Server is listening")
+);
 var io = require("socket.io")(server);
 //////////////////////////////////////////////END_SETUP_SERVER/////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-app.get('/', (req, res) => { // <=> app.get('/', function(req, res){
+app.get("/", (req, res) => {
+  // <=> app.get('/', function(req, res){
   //res.send("Home page. Server chạy ngon lành cành đào");//thường ở đây sẽ chèn html vào để chạy web
-  res.render("trangchu");//dùng farmework ejs để build html trangchu.ejs ra
+  res.render("trangchu"); //dùng farmework ejs để build html trangchu.ejs ra
 });
 
 ////////////////////////////SOKET_IO//////////////////////////////////////////////////
-io.on("connection", function(socket){//khi có người dùng truy cập web thì "connection" sẽ đc truyền qua
+io.on("connection", function (socket) {
+  //khi có người dùng truy cập web thì "connection" sẽ đc truyền qua
   console.log("socket nè: Có người kết nối: " + socket.id);
   //----------------------------------------------
   //nguoi dung ngat ket noi vào trang web
-  socket.on("disconnect", function(){
-      console.log(socket.Username + " đã ngắt kết nối rồi nè!!!");
+  socket.on("disconnect", function () {
+    console.log(socket.Username + " đã ngắt kết nối rồi nè!!!");
   });
 });
 ////////////////////////////END_SOKET_IO//////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Adds support for GET/POST requests to our webhook -> của FB Messenger////////////////////////////////////////
-app.get('/webhook', (req, res) => {
-
+app.get("/webhook", (req, res) => {
   // Your verify token. Should be a random string.
   let VERIFY_TOKEN = process.env.VALIDATION_TOKEN;
   console.log("VAO NEEEE");
   // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-    
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
-  
     // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
       // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
+      console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
-    
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
       //console.log('THAT_BAIIIII');
-      res.sendStatus(403);      
+      res.sendStatus(403);
     }
   }
 });
 
 app.post("/webhook", (req, res) => {
-    //console.log(req);
+  //console.log(req);
 
-    let body = req.body;
-    //console.log(body);
-    // Checks this is an event from a page subscription
-    if (body.object === 'page') {
-      body.entry.forEach(function(entry) {
+  let body = req.body;
+  //console.log(body);
+  // Checks this is an event from a page subscription
+  if (body.object === "page") {
+    body.entry.forEach(function (entry) {
+      let webhook_event = entry.messaging[0];
 
-        let webhook_event = entry.messaging[0];
+      let sender_psid = webhook_event.sender.id;
+      //console.log('Sender PSID: ' + sender_psid);
+      console.log("EVEN VAOPOOOOOOOOO!!!!!!!");
+      console.log(webhook_event);
 
-        let sender_psid = webhook_event.sender.id;
-        //console.log('Sender PSID: ' + sender_psid);
-        console.log('EVEN VAOPOOOOOOOOO!!!!!!!');
-        console.log(webhook_event);
+      if (webhook_event.message) {
+        xuly.handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        //(2)
+        xuly.handlePostback(sender_psid, webhook_event.postback);
+      }
+    });
 
-        if (webhook_event.message) {
-          xuly.handleMessage(sender_psid, webhook_event.message);        
-        } else if (webhook_event.postback) {//(2)
-          xuly.handlePostback(sender_psid, webhook_event.postback);
-        }
-      });
-  
-      // Returns a '200 OK' response to all requests
-      res.status(200).send('EVENT_RECEIVED');
-    } else {
-      // Returns a '404 Not Found' if event is not from a page subscription
-      res.sendStatus(404);
-    }
+    // Returns a '200 OK' response to all requests
+    res.status(200).send("EVENT_RECEIVED");
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
 });
 // END// Adds support for GET/POST requests to our webhook -> của FB Messenger////////////////////////////////////////
-
-
--- xuly/index.js
+++ xuly/index.js
@@ -1,180 +1,177 @@
-require('dotenv').config();
-const request = require('request');
-module.exports = { //chìa ra function() để server.js khác có thể reques và dùng ....
+require("dotenv").config();
+const request = require("request");
+module.exports = {
+  //chìa ra function() để server.js khác có thể reques và dùng ....
   handleMessage: handleMessage,
   handlePostback: handlePostback,
   callSendAPI: callSendAPI,
 };
-function callSendAPI(style, response) {
-  //console.log('Sender PSID by callSendAPI: ' + sender_psid);
-  // Send the HTTP request to the Messenger Platform
-  request({
-    "uri": "https://graph.facebook.com/v7.0/me/" + style,
-    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-    "method": "POST",
-    "json": response
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message: ' + response +' ĐÃ ĐƯỢC GỬI!: ' +err);
-    } else {
-      console.error("THẤT BẠI to send message: " + err);
-    }
-  }); 
-}
 
-function STARTED(sender_psid){
+function STARTED(sender_psid) {
   //console.log('Vao <postback_payload> NÈNÈ!!!!!!!!!!');
 
-    // sender: { id: '3006492652803294' },
-    // recipient: { id: '104124098046144' },
-    // timestamp: 1596112909237,
-    // postback: { title: 'Get Started', payload: 'GET_STARTED_PAYLOAD' }
-    response = { 
-      //"text": `Xin chào "${{user_full_name}}!", Bạn cần làm gì?`,
-      //"text":"What do you want to do next?",
-        "recipient":{
-          "id": sender_psid
+  // sender: { id: '3006492652803294' },
+  // recipient: { id: '104124098046144' },
+  // timestamp: 1596112909237,
+  // postback: { title: 'Get Started', payload: 'GET_STARTED_PAYLOAD' }
+  response = {
+    //"text": `Xin chào "${{user_full_name}}!", Bạn cần làm gì?`,
+    //"text":"What do you want to do next?",
+    recipient: {
+      id: sender_psid,
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "button",
+          text:
+            "Chào mừng bạn đến với DOVANBOT, xin lựa chọn chức năng bạn cần.\n Input - nhập danh sách mã lớp học để lấy hình thời khóa biểu của bạn \n Sign in/up - để đồng bộ data với web",
+          buttons: [
+            {
+              type: "postback",
+              title: " ⌨ Input danh sách",
+              payload: "danhsach_monhoc",
+            },
+            {
+              type: "postback",
+              title: "🏷 Sign in/up",
+              payload: "login_ne",
+            },
+            {
+              type: "web_url",
+              url: "https://dovanbot2.herokuapp.com/",
+              title: "🐥 Trang chủ",
+            },
+          ],
         },
-        "message":{
-          "attachment":{
-            "type":"template",
-            "payload":{
-              "template_type":"button",
-              "text":"Chào mừng bạn đến với DOVANBOT, xin lựa chọn chức năng bạn cần.\n Input - nhập danh sách mã lớp học để lấy hình thời khóa biểu của bạn \n Sign in/up - để đồng bộ data với web",
-              "buttons":[
-                {
-                  "type":"postback",
-                  "title":" ⌨ Input danh sách",
-                  "payload":"danhsach_monhoc"
-                },
-                {
-                  "type":"postback",
-                  "title":"🏷 Sign in/up",
-                  "payload":"login_ne"
-                },
-                {
-                  "type":"web_url",
-                  "url":"https://dovanbot2.herokuapp.com/",
-                  "title":"🐥 Trang chủ"
-                },
-              ]
-            }
-          }
-        }
-    }
-    callSendAPI('messages', response);
+      },
+    },
+  };
+  callSendAPI("messages", response);
 }
 function HuongDan(sender_psid) {
   STARTED(sender_psid);
 }
 
-function handleMessage(sender_psid, received_message) {
-    // var message = received_message;
-    var isEcho = received_message.is_echo;
-    var messageId = received_message.mid;
-    var appId = received_message.app_id;
-    var metadata = received_message.metadata;
-  
-    // // You may get a text or attachment but not both
-    var messageText = received_message.text;
-    // var messageAttachments = received_message.attachments;
-    // var quickReply = received_message.quick_reply;
-
-
-
-
-    let response;// response is a JSON
-    if (received_message.text) {// Check if the message contains text
-      // Create the payload for a basic text message
-      response = {
-        "recipient": {
-          "id": sender_psid
-        },
-        "message": {
-          "text": `You sent the message: "${received_message.text}".`
-        }
+function callSendAPI(style, response) {
+  //console.log('Sender PSID by callSendAPI: ' + sender_psid);
+  // Send the HTTP request to the Messenger Platform
+  request(
+    {
+      uri: "https://graph.facebook.com/v7.0/me/" + style,
+      qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: response,
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("message: " + response + " ĐÃ ĐƯỢC GỬI!: " + err);
+      } else {
+        console.error("THẤT BẠI to send message: " + err);
       }
-      callSendAPI('messages', response);// Sends the response message
     }
-    else if (received_message.attachments) {//(2)
-      //https://developers.facebook.com/docs/messenger-platform/getting-started/quick-start#setup-complete
-      // Gets the URL of the message attachment
-      let attachment_url = received_message.attachments[0].payload.url;
-      response = {
-        "recipient": {
-          "id": sender_psid
-        },
-        "message":{
-          "attachment": {
-            "type": "template",
-            "payload": {
-              "template_type": "generic",
-              "elements": [{
-                "title": "Is this the right picture?",
-                "subtitle": "Tap a button to answer.",
-                "image_url": attachment_url,
-                "buttons": [
+  );
+}
+function handleMessage(sender_psid, received_message) {
+  // var message = received_message;
+  var isEcho = received_message.is_echo;
+  var messageId = received_message.mid;
+  var appId = received_message.app_id;
+  var metadata = received_message.metadata;
+
+  // // You may get a text or attachment but not both
+  var messageText = received_message.text;
+  // var messageAttachments = received_message.attachments;
+  // var quickReply = received_message.quick_reply;
+
+  let response; // response is a JSON
+  if (received_message.text) {
+    // Check if the message contains text
+    // Create the payload for a basic text message
+    response = {
+      recipient: {
+        id: sender_psid,
+      },
+      message: {
+        text: `You sent the message: "${received_message.text}".`,
+      },
+    };
+    callSendAPI("messages", response); // Sends the response message
+  } else if (received_message.attachments) {
+    //(2)
+    //https://developers.facebook.com/docs/messenger-platform/getting-started/quick-start#setup-complete
+    // Gets the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      recipient: {
+        id: sender_psid,
+      },
+      message: {
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "generic",
+            elements: [
+              {
+                title: "Is this the right picture?",
+                subtitle: "Tap a button to answer.",
+                image_url: attachment_url,
+                buttons: [
                   {
-                    "type": "postback",
-                    "title": "OKOKO!",
-                    "payload": "yes",
+                    type: "postback",
+                    title: "OKOKO!",
+                    payload: "yes",
                   },
                   {
-                    "type": "postback",
-                    "title": "No!",
-                    "payload": "no",
-                  }
+                    type: "postback",
+                    title: "No!",
+                    payload: "no",
+                  },
                 ],
-              }]
-            }
-          }
-        }
-      }
-      
-      callSendAPI('messages', response);// Sends the response message
-    } 
-    else if (received_message.quickReply) {
-    }
+              },
+            ],
+          },
+        },
+      },
+    };
+
+    callSendAPI("messages", response); // Sends the response message
+  } else if (received_message.quickReply) {
+  }
 }
 function handlePostback(sender_psid, received_postback) {
-  let response;// response is a JSON
-  
+  let response; // response is a JSON
+
   // Get the payload for the postback
   let payload = received_postback.payload;
   switch (payload) {
-    case 'yes':
-      response = { 
-        "recipient": {
-          "id": sender_psid
+    case "yes":
+      response = {
+        recipient: {
+          id: sender_psid,
         },
-        "message": {"text": "Thanks!"} 
-      }
-      callSendAPI('messages', response);
+        message: { text: "Thanks!" },
+      };
+      callSendAPI("messages", response);
       break;
-    case 'no':
-      response = { 
-        "recipient": {
-          "id": sender_psid
+    case "no":
+      response = {
+        recipient: {
+          id: sender_psid,
         },
-        "message": {"text": "Oops, try sending another image." }
-      }
-      callSendAPI('messages', response);
+        message: { text: "Oops, try sending another image." },
+      };
+      callSendAPI("messages", response);
       break;
-    case 'GET_STARTED_PAYLOAD':
+    case "GET_STARTED_PAYLOAD":
       STARTED(sender_psid);
       break;
-    case 'huong_dan':
+    case "huong_dan":
       HuongDan(sender_psid);
       break;
 
     default:
       break;
   }
-  
 }
-
-
-
-
-
-
