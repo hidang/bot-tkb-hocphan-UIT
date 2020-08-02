f31a7cd019590ee4cc4958bb958ed47a84e88fac
hidang <yellowstudio.vn@gmail.com>
2020-08-02 21:50:03 +0700

    byDang

--- server.js
+++ server.js
@@ -42,7 +42,6 @@ function FINDtoADDID(sender_psid) {
   var dbo = client.db("dovanbot");
   dbo.collection("user").findOne({ _id: sender_psid }, function (err, result) {
     if (err) throw err;
-    console.log(result);
     if (result == null) {
       kq = false;
     } else {
