d4cbc9df3c08fbd48f159e313eab30331c7ca745
hidang <yellowstudio.vn@gmail.com>
2020-08-05 21:06:49 +0700

    byDang

--- server.js
+++ server.js
@@ -22,8 +22,9 @@ const uri = process.env.URI_NE;
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 //////////////////////////////////////////////END_SETUP_SERVER/////////////////////////////////////////////////
-// let type_typing = 0;
-
+let thaotac_excel = require("./thaotac_excel.js");
+// let kq = thaotac_excel.get_Malop("E10");
+// console.log(kq.data.malop);
 /////////////////////////TODO: MongoDB/////////////////////////////////////////////////////////////
 client.connect((err) => {
   if (err) throw err;
@@ -120,83 +121,6 @@ let getTypeTyping = function (sender_psid, callback) {
   });
 };
 /////////////////////////END_MongoDB/////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
-app.get("/", (req, res) => {
-  // <=> app.get('/', function(req, res){
-  //res.send("Home page. Server chạy ngon lành cành đào");//thường ở đây sẽ chèn html vào để chạy web
-  res.render("trangchu"); //dùng farmework ejs để build html trangchu.ejs ra
-});
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-////////////////////////////TODO:SOKET_IO//////////////////////////////////////////////////
-io.on("connection", function (socket) {
-  //khi có người dùng truy cập web thì "connection" sẽ đc truyền qua
-  console.log("socket nè: Có người kết nối: " + socket.id);
-  //----------------------------------------------
-  //nguoi dung ngat ket noi vào trang web
-  socket.on("disconnect", function () {
-    console.log(socket.id + " đã ngắt kết nối rồi nè!!!");
-  });
-  //console.log(FINDtoADDID("123"));
-});
-////////////////////////////END_SOKET_IO//////////////////////////////////////////////////
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Adds support for GET/POST requests to our webhook -> của FB Messenger////////////////////////////////////////
-app.get("/webhook", (req, res) => {
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = process.env.VALIDATION_TOKEN;
-  // Parse the query params
-  let mode = req.query["hub.mode"];
-  let token = req.query["hub.verify_token"];
-  let challenge = req.query["hub.challenge"];
-
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-    // Checks the mode and token sent is correct
-    if (mode === "subscribe" && token === VERIFY_TOKEN) {
-      // Responds with the challenge token from the request
-      //console.log("WEBHOOK_VERIFIED");
-      res.status(200).send(challenge);
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      //console.log('THAT_BAIIIII');
-      res.sendStatus(403);
-    }
-  }
-});
-
-app.post("/webhook", (req, res) => {
-  //console.log(req);
-
-  let body = req.body;
-  //console.log(body);
-  // Checks this is an event from a page subscription
-  if (body.object === "page") {
-    body.entry.forEach(function (entry) {
-      let webhook_event = entry.messaging[0];
-
-      let sender_psid = webhook_event.sender.id;
-      //console.log('Sender PSID: ' + sender_psid);
-      console.log("webhook EVEN VAOPOOOOOOOOO!!!!!!!");
-      //TODO:console.log(webhook_event);
-
-      if (webhook_event.message) {
-        handleMessage(sender_psid, webhook_event.message);
-      } else if (webhook_event.postback) {
-        //(2)
-        handlePostback(sender_psid, webhook_event.postback);
-      }
-    });
-
-    // Returns a '200 OK' response to all requests
-    res.status(200).send("EVENT_RECEIVED");
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-});
-// END// Adds support for GET/POST requests to our webhook -> của FB Messenger////////////////////////////////////////
 //////////////////////TODO:EVENT_MESSENGER////////////////////////////////////////////////////////////////
 function handleMessage(sender_psid, received_message) {
   let response; // response is a JSON
@@ -523,3 +447,80 @@ function LOGIN(sender_psid) {
 function LOGOUT(sender_psid) {}
 
 //////////////////////END:EVENT_MESSENGER////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+app.get("/", (req, res) => {
+  // <=> app.get('/', function(req, res){
+  //res.send("Home page. Server chạy ngon lành cành đào");//thường ở đây sẽ chèn html vào để chạy web
+  res.render("trangchu"); //dùng farmework ejs để build html trangchu.ejs ra
+});
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////SOKET_IO//////////////////////////////////////////////////
+io.on("connection", function (socket) {
+  //khi có người dùng truy cập web thì "connection" sẽ đc truyền qua
+  console.log("socket nè: Có người kết nối: " + socket.id);
+  //----------------------------------------------
+  //nguoi dung ngat ket noi vào trang web
+  socket.on("disconnect", function () {
+    console.log(socket.id + " đã ngắt kết nối rồi nè!!!");
+  });
+  //console.log(FINDtoADDID("123"));
+});
+////////////////////////////END_SOKET_IO//////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Adds support for GET/POST requests to our webhook -> của FB Messenger////////////////////////////////////////
+app.get("/webhook", (req, res) => {
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = process.env.VALIDATION_TOKEN;
+  // Parse the query params
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Checks the mode and token sent is correct
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      // Responds with the challenge token from the request
+      //console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      //console.log('THAT_BAIIIII');
+      res.sendStatus(403);
+    }
+  }
+});
+
+app.post("/webhook", (req, res) => {
+  //console.log(req);
+
+  let body = req.body;
+  //console.log(body);
+  // Checks this is an event from a page subscription
+  if (body.object === "page") {
+    body.entry.forEach(function (entry) {
+      let webhook_event = entry.messaging[0];
+
+      let sender_psid = webhook_event.sender.id;
+      //console.log('Sender PSID: ' + sender_psid);
+      console.log("webhook EVEN VAOPOOOOOOOOO!!!!!!!");
+      //TODO:console.log(webhook_event);
+
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        //(2)
+        handlePostback(sender_psid, webhook_event.postback);
+      }
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send("EVENT_RECEIVED");
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+});
+// END// Adds support for GET/POST requests to our webhook -> của FB Messenger////////////////////////////////////////
---
+++ thaotac_excel.js
@@ -0,0 +1,22 @@
+// const XLSX = require("xlsx");
+// const workbook = XLSX.readFile("lichthi_l2_hk2_nh2019_2020.xls");
+
+// module.exports.get_Malop = (index) => {
+//   var first_sheet_name = workbook.SheetNames[0];
+//   var worksheet = workbook.Sheets[first_sheet_name]; //trang tinh'
+//   var address_of_cell = index;
+//   var desired_cell = worksheet[address_of_cell];
+//   var desired_value = desired_cell ? desired_cell.v : undefined;
+
+//   let response;
+//   response = {
+//     data: {
+//       malop: desired_value,
+//     },
+//   };
+//   return response;
+// };
+
+// module.exports.get_Tenmonhoc = () => {
+//   // Code here
+// };
