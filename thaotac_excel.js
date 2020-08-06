04533582adbbf1c25f11d25ac4b9c6f0f1390f1e
hidang <yellowstudio.vn@gmail.com>
2020-08-06 09:49:18 +0700

    byDang

--- thaotac_excel.js
+++ thaotac_excel.js
@@ -1,22 +1,23 @@
-// const XLSX = require("xlsx");
-// const workbook = XLSX.readFile("lichthi_l2_hk2_nh2019_2020.xls");
+const XLSX = require("xlsx");
+const workbook = XLSX.readFile(
+  "TKB_KHDT_04-08-2020_1596503345_HK_1_NH2020.xlsx"
+);
 
-// module.exports.get_Malop = (index) => {
-//   var first_sheet_name = workbook.SheetNames[0];
-//   var worksheet = workbook.Sheets[first_sheet_name]; //trang tinh'
-//   var address_of_cell = index;
-//   var desired_cell = worksheet[address_of_cell];
-//   var desired_value = desired_cell ? desired_cell.v : undefined;
+module.exports.get_Malop = (index) => {
+  var first_sheet_name = workbook.SheetNames[0];
+  var worksheet = workbook.Sheets[first_sheet_name]; //trang tinh'
+  var address_of_cell = index;
+  var desired_cell = worksheet[address_of_cell];
+  var desired_value = desired_cell ? desired_cell.v : undefined;
+  let response;
+  response = {
+    data: {
+      malop: desired_value,
+    },
+  };
+  return response;
+};
 
-//   let response;
-//   response = {
-//     data: {
-//       malop: desired_value,
-//     },
-//   };
-//   return response;
-// };
-
-// module.exports.get_Tenmonhoc = () => {
-//   // Code here
-// };
+module.exports.get_Tenmonhoc = () => {
+  // Code here
+};
