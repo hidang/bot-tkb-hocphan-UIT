50e99f580bc931cf9261ac651028bc15faacabbc
hidang <yellowstudio.vn@gmail.com>
2020-08-06 20:09:01 +0700

    byDang

--- thaotac_excel.js
+++ thaotac_excel.js
@@ -1,9 +1,21 @@
-const XLSX = require("xlsx");
-const workbook = XLSX.readFile(
-  "TKB_KHDT_04-08-2020_1596503345_HK_1_NH2020.xlsx"
-);
+let server = require("./server.js");
+var XLSX = require("xlsx");
+var workbook = XLSX.readFile("TKB_KHDT_04-08-2020_1596503345_HK_1_NH2020.xlsx");
 
 module.exports.get_Malop = (index) => {
+  var dbo = server.client.db("dovanbot");
+  dbo.collection("user").findOne({ _id: "123456" }, function (err, result) {
+    if (err) throw err;
+    //console.log(result);
+    //console.log(result._id);
+    //var resultt = result._id;
+    if (result == null) {
+      //console.log("false -> add");
+      them_id(sender_psid);
+    } else {
+      console.log("OK tHAOTAC EXCEL");
+    }
+  });
   var first_sheet_name = workbook.SheetNames[0];
   var worksheet = workbook.Sheets[first_sheet_name]; //trang tinh'
   var address_of_cell = index;
@@ -19,17 +31,17 @@ module.exports.get_Malop = (index) => {
 };
 
 module.exports.set_Code_Class = (CODE_CLASS) => {
-  var code_suscess = [];
+  var code_suscess_data = [];
+  var response = {};
   var code_error = [];
-  //xu ly ne
   var n = CODE_CLASS.length;
   for (var index = 0; index < n; index++) {
     console.log(CODE_CLASS[index]);
   }
-  let response;
+
   response = {
     data: {
-      code_suscess: code_suscess,
+      code_suscess: code_suscess_data,
     },
     error: {
       code_error: code_error,
