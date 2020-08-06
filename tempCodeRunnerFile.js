ae80a4128521b43a9ad707ea0e816d6980adf6c5
hidang <yellowstudio.vn@gmail.com>
2020-08-06 22:19:29 +0700

    byDang

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,11 +1,1 @@
-  var dbo = client.db("dovanbot");
-  dbo.collection("user").findOne({ _id: sender_psid }, function (err, result) {
-    if (err) throw err;
-    //console.log(result);
-    //console.log(result._id);
-    //var resultt = result._id;
-    if (result == null) {
-      //console.log("false -> add");
-      them_id(sender_psid);
-    }
-  });
+kq_code_class
