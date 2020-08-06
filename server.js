85bb80c96a00caf96582778a4f0d74037cd6a8ba
hidang <yellowstudio.vn@gmail.com>
2020-08-06 15:32:13 +0700

    byDang

--- server.js
+++ server.js
@@ -182,7 +182,7 @@ function handleMessage(sender_psid, received_message) {
           //console.log(pos);
           // var content = received_message.text.substr(pos);
           // console.log(content);
-          let CODE_CLASS = [];
+          var CODE_CLASS = [];
           var temp = 0;
           var n = received_message.text.length;
           //console.log(n);
@@ -199,7 +199,7 @@ function handleMessage(sender_psid, received_message) {
             // for (let index = 0; index < CODE_CLASS.length; index++) {
             //   console.log(CODE_CLASS[index]);
             // }
-            let kq_code_class = thaotac_excel.set_Code_Class(CODE_CLASS, n);
+            var kq_code_class = thaotac_excel.set_Code_Class(CODE_CLASS, n);
             console.log(kq_code_class);
             //input danh sách thành công bạn có muốn lấy hình ảnh thời khóa biểu của bạn ngay bây giờ? -> câu trả lời nhanh
             ChangeTypeTyping(sender_psid, "input_khong");
