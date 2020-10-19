b65986c0d59960f407be5ea4f89e58bce3b814d9
hidang <yellowstudio.vn@gmail.com>
2020-10-19 20:19:04 +0700

    byDang

--- sources/handle_message/handlePostback.js
+++ sources/handle_message/handlePostback.js
@@ -1,9 +1,9 @@
 const STARTED          = require('../botFunction/STARTED');
 const HuongDan         = require('../botFunction/HuongDan');
-const getTypeTyping    = require('../botFunction/getTypeTyping');
-const Change_username  = require('../botFunction/Change_username');
-const Change_password  = require('../botFunction/Change_password');
-const ChangeTypeTyping = require('../botFunction/ChangeTypeTyping');
+// const getTypeTyping    = require('../botFunction/getTypeTyping');
+// const Change_username  = require('../botFunction/Change_username');
+// const Change_password  = require('../botFunction/Change_password');
+// const ChangeTypeTyping = require('../botFunction/ChangeTypeTyping');
 const CHUAHOANTHANH    = require('../botFunction/chuahoanthanh');
 const callSendAPI      = require('../useAPI/callSendAPI');
 module.exports.handlePostback = function (sender_psid, received_postback) {
@@ -37,13 +37,13 @@ module.exports.handlePostback = function (sender_psid, received_postback) {
       HuongDan(sender_psid);
       break;
     case "login":
-      LOGIN(sender_psid);
+      //LOGIN(sender_psid);
       break;
     case "logout":
-      LOGOUT(sender_psid);
+      //LOGOUT(sender_psid);
       break;
     case "input_code_class":
-      INPUT_CODE_CLASS(sender_psid);
+      //INPUT_CODE_CLASS(sender_psid);
       break;
     default:
       CHUAHOANTHANH(sender_psid);
