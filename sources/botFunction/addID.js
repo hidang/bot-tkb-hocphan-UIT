18ffae12c12a16dbff9b60ac801e70df54ac6d3d
hidang <yellowstudio.vn@gmail.com>
2020-12-01 18:26:37 +0700

    bydang

--- sources/botFunction/addID.js
+++ sources/botFunction/addID.js
@@ -5,6 +5,6 @@ module.exports.FINDtoADDID = (sender_psid) => {
     if(result) console.log(sender_psid +'- đã được thêm thành công! *addIDs');
     //có 2 TH lỗi ở đây: 
     else if (err === null) console.log(sender_psid +'- đã tồn tại!'); //Đã tồn tại
-      else if(err !== null) console.log('*addID.js them id mới thất bại: !'+ err);//database lỗi
+      else /*if(err !== null)*/ console.log('*addID.js them id mới thất bại: !'+ err);//database lỗi
   });
 }
