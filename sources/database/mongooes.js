f4e3f26569f2c3f657d3783b761af344e68bd97d
hidang <yellowstudio.vn@gmail.com>
2020-12-01 17:57:13 +0700

    bydang

---
+++ sources/database/mongooes.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+function conect() {
+  mongoose.connect(require("../../config/app").db.connectionUri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  });
+}
+module.exports = {
+  conect: conect,
+  
+};
