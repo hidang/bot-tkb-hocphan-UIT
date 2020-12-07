6f5b8fbae33bd68386725f252d7df16bf80a7a7c
hidang <yellowstudio.vn@gmail.com>
2020-12-07 23:07:03 +0700

    byhidang

--- sources/database/mongooes.js
+++ sources/database/mongooes.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
-async function conect() {
+async function conect() {//Phải sử dụng async/await nếu không bot sẽ crash khi không kết nối được database
   try {
     await mongoose.connect(require("../../config/app").db.connectionUri, {
       useUnifiedTopology: true,
       useNewUrlParser: true
     });
-  } catch (error) {//TODO: send message thông báo user lỗi database
+  } catch (error) {
     console.log('LOI KET NOI DATABASE:' +error);//TODO: send message thông báo user lỗi database
   }
 }
