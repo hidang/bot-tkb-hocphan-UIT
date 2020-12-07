7cdad1b8307090c9a40ceaf7889f7de50470af06
hidang <yellowstudio.vn@gmail.com>
2020-12-08 00:18:25 +0700

    byhidang

--- sources/database/mongooes.js
+++ sources/database/mongooes.js
@@ -5,10 +5,10 @@ async function conect() {//Phải sử dụng async/await nếu không bot sẽ 
       useUnifiedTopology: true,
       useNewUrlParser: true
     });
+    return "hidang";
   } catch (error) {
     console.log('LOI KET NOI DATABASE:' +error);//TODO: send message thông báo user lỗi database
-    //return error;
-    //object Promise { MongoParseError:: }
+    return error;//object Promise { MongoParseError:: }
   }
 }
 module.exports = {
