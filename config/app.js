47342d8bf54e5f1e3ee1e870b2fa7802eb3e1e89
hidang <yellowstudio.vn@gmail.com>
2020-12-07 22:43:37 +0700

    byhidang

--- config/app.js
+++ config/app.js
@@ -9,7 +9,7 @@ module.exports = {
   author: "hidang",
   version: "2.0",
   db: {
-    connectionUri: process.env.URI_NEE,
+    connectionUri: 'hidangdeptrai',//process.env.URI_NE,
     params: {},
     collections: ["user"]
   }
