8a07b0cac28e4555b7971338ca646b110741c2fb
hidang <yellowstudio.vn@gmail.com>
2020-12-08 20:03:20 +0700

    byhidang

--- sources/botFunction/addID.js
+++ sources/botFunction/addID.js
@@ -4,7 +4,7 @@ module.exports.FINDtoADDID = (sender_psid) => {
   db_user.createNew(sender_psid, (err, result) => {
     if(result) console.log(sender_psid +'- đã được thêm thành công! *addIDs');
     else //có 2 TH lỗi ở đây: 
-      if (err === null) console.log(sender_psid +'- đã tồn tại!'); //result === false
+      if (err === null) console.log(sender_psid +'- đã tồn tại!'); //TODO:send message chào mừng quay lại
         else /*if(err !== null)*/ {
           //TODO:send message đến user ib
           console.log('*addID.js them id mới thất bại: ! - '+ err);//database lỗi
