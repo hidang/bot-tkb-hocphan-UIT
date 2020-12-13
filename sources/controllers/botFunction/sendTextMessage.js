7a75e19c6b388a61758ca19b164a8ac8e39b7840
hidang <yellowstudio.vn@gmail.com>
2020-12-13 21:28:41 +0700

    byhidang

---
+++ sources/controllers/botFunction/sendTextMessage.js
@@ -0,0 +1,12 @@
+const FB_API = require('../../useAPI/FB_API');
+module.exports.sendTextMessage = (sender_psid, text) =>{
+  var response = {
+    recipient: {
+      id: sender_psid,
+    },
+    message: {
+      text: text,
+    },
+  };
+  FB_API.callSendAPI("messages", response); // Sends the response message
+}
