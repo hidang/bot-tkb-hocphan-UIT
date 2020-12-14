4d9ccf93cd3a56b8537d1a222625b3ce5f82041d
hidang <yellowstudio.vn@gmail.com>
2020-12-14 12:37:17 +0700

    byhidang

--- node_modules/messaging-api-messenger/src/__tests__/browser.spec.js
+++
@@ -1,7 +0,0 @@
-import { Messenger, MessengerBatch, MessengerBroadcast } from '../browser';
-
-it('should export api correctly', () => {
-  expect(Messenger).toBeDefined();
-  expect(MessengerBatch).toBeDefined();
-  expect(MessengerBroadcast).toBeDefined();
-});
