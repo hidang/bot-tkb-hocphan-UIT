a985dfd001c95a82c7d38fa50aaa7938ac25fd9f
hidang <yellowstudio.vn@gmail.com>
2020-12-14 11:44:36 +0700

    byhidang

--- sources/controllers/botFunction/changeTypeTyping.js
+++ sources/controllers/botFunction/changeTypeTyping.js
@@ -1,8 +1,9 @@
 const db_user = require('../../utils/handlers/users');
 module.exports.ChangeTypeTyping = (sender_psid, typing) => {
   db_user.updateCodeClass(typing, sender_psid, (err, result) =>{
-    if(!err)
-      console.log(sender_psid +'- đã update thành công!' + typing);
+    if(!err){
+      //console.log(sender_psid +'- đã update thành công!' + typing);
+    }
     else {
       //database lỗi->TODO:send message 
       console.log('*ChangeTypeTyping.js update thất bại: ! - '+ err);
