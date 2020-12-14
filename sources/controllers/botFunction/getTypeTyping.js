6457b96e931b76e71bb7fe3ecd5962b7edb33bea
hidang <yellowstudio.vn@gmail.com>
2020-12-14 08:52:28 +0700

    byhidang

--- sources/controllers/botFunction/getTypeTyping.js
+++ sources/controllers/botFunction/getTypeTyping.js
@@ -1,13 +1,13 @@
 const db_user = require('../../utils/handlers/users');
 
-module.exports.getTypeTyping = (sender_psid, cb) =>{
+module.exports.getTypeTyping = (sender_psid) =>{
   db_user.getTypeTyping(sender_psid, (err, result) =>{
     if(!err) {
       console.log(sender_psid +'- đã get thành công!' + result);
-      return cb(result);
+      return result;
     }else {
       console.log('*getTypeTyping.js get data thất bại: ! - '+ err);
-      return cb(false);
+      return false;
     }
   });
 };
