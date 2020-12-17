77333d3eefb925beb9783844f2461ea423180292
hidang <yellowstudio.vn@gmail.com>
2020-12-17 23:05:39 +0700

    byhidang

--- public/soantkbhp.js
+++ public/soantkbhp.js
@@ -1,6 +1,44 @@
+var listCheckBox = [
+  'STT', 'MaMH', 'MaLop', 'TenMH', 'MaGV', //1->5
+  'TenGV', 'SiSo', 'SoTC', 'ThucHanh', 'HTGD',//6->10
+  'Thu', 'Tiet', 'CachTuan', 'PhongHoc', 'KhoaHoc', //11->15
+  'HocKy', 'NamHoc', 'HeDT', 'KhoaQL', 'NBD', //16->20
+  'NKT', 'GhiChu'//21, 22
+];
+
+var listElementsCheckBox = [];//Mảng các element-checbox
+listCheckBox.forEach(element => {
+  listElementsCheckBox.push(document.getElementById(element));
+});
+listElementsCheckBox.forEach(element => {
+  element.addEventListener('click', ()=>{
+    ShowOrHideCol(element);
+  });
+});
+listElementsCheckBox.forEach(element => {
+  ShowOrHideCol(element);
+});
+
+function ShowOrHideCol(elementCheckBox) {
+  console.log(elementCheckBox.id);
+  var ShowOrHide = 'none';//hide
+  if (elementCheckBox.checked) ShowOrHide = '';//show
+
+  var listCell = document.getElementsByName(`cell-${elementCheckBox.id}`); 
+
+  listCell.forEach(element =>{
+    element.style.display  = ShowOrHide;
+  });
+}
+
+
+
+
+
+
 const TableSelect = document.getElementById('TableSelect');
-
-
+const body_table  = document.getElementById('body-table');
+start();
 
 
 function readTextFile(file) {
@@ -20,8 +58,8 @@ function readTextFile(file) {
 }
 async function start() {
   var jsondata = await readTextFile("./tkbhp.json");
-  data_tkb = JSON.parse(jsondata);
-  console.log(data_tkb.data[1]);
+  var data_json = JSON.parse(jsondata);
+  var data_tkb = data_json.data;
+  console.log(data_tkb[8]);
 }
 
-start();
