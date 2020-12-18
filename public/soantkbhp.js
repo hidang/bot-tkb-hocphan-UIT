782b596d1b04d2c5184f8ee366089bcfd5c2cf6b
hidang <yellowstudio.vn@gmail.com>
2020-12-18 23:24:20 +0700

    byhidang

--- public/soantkbhp.js
+++ public/soantkbhp.js
@@ -1,35 +1,36 @@
-const listColumns = [
-  'STT', 'MaMH', 'MaLop', 'TenMH', 'MaGV', //1->5
-  'TenGV', 'SiSo', 'SoTc', 'ThucHanh', 'HTGD',//6->10
-  'Thu', 'Tiet', 'CachTuan', 'PhongHoc', 'KhoaHoc', //11->15
-  'HocKy', 'NamHoc', 'HeDT', 'KhoaQL', 'NBD', //16->20
-  'NKT', 'GhiChu'//21, 22
+const listColumns = [//22
+  'STT', 'MaMH', 'MaLop', 'TenMH', 'MaGV',
+  'TenGV', 'SiSo', 'SoTc', 'ThucHanh', 'HTGD',
+  'Thu', 'Tiet', 'CachTuan', 'PhongHoc', 'KhoaHoc',
+  'HocKy', 'NamHoc', 'HeDT', 'KhoaQL', 'NBD',
+  'NKT', 'GhiChu'
 ];
-const container = document.getElementById("container");
-const start_data = document.getElementById("start-data");
+const container = document.getElementById('container');
+const start_data = document.getElementById('start-data');
 //const TableSelect = document.getElementById('TableSelect');
 const body_table  = document.getElementById('body-table');
 const danhsach_selected = document.getElementById('danhsach-selected');
 const info_danhsach_selected = document.getElementById('info-danhsach-selected');
-const status_text_info =document.getElementById('status-text-info');
+//const status_text_info =document.getElementById('status-text-info');
+const show_list_malop = document.getElementById('show-list-malop');
+const show_TongTC = document.getElementById('show-TongTC');
 var data_tkb = '';//Object dữ liệu từ file excel tất cả môn học
 var MyCodeClassList = [];//Danh sách {info} lớp học đã chọn -> đã check trùng lịch...
 var listElementsCheckBox = [];//Mảng các element-checbox-Loc
-
+var TongTc = 0;//số tính chỉ của danh sách đang chọn
 //push elementCheckBox to array
 listColumns.forEach(element => {
   ////set size ALL BOX text-input 
   //document.getElementById(`cell-${element}`).size = 10;
   listElementsCheckBox.push(document.getElementById(element));
 });
-
 //set envent show/hide for elementCheckBoxs
 listElementsCheckBox.forEach(element => {
   element.addEventListener('click', ()=>{
     ShowOrHideCol(element);
   });
 });
-
+//---------------------------------------------------------------------------------------------------------------
 function ShowOrHideCol(elementCheckBox) {
   //=>if checked ? show:hide -> elements
   var ShowOrHide = 'none';//hide
@@ -39,8 +40,7 @@ function ShowOrHideCol(elementCheckBox) {
     element.style.display  = ShowOrHide;
   });
 }
-
-function ReadJsonFile(file) {
+function ReadJsonFile(file) {//return Promise resolve -> data in file json
   return new Promise(
     function (resolve) {
       var rawFile = new XMLHttpRequest();
@@ -55,7 +55,7 @@ function ReadJsonFile(file) {
     }
   )
 }
-function GetInfoClassByMaLop(malop) {
+function GetInfoClassByMaLop(malop) {//return object info_lop
   var infoLop = '';
   if(data_tkb){
     for (const datalop of data_tkb) {
@@ -69,42 +69,66 @@ function GetInfoClassByMaLop(malop) {
   }
   return infoLop;
 }
-function InnerData2List(info_lop) {
+function handle_show_list_malop() {
+  var list_malop_show = '';
+  MyCodeClassList.forEach(element => {
+    list_malop_show += `${element}</br>`;
+  });
+  show_list_malop.innerHTML = list_malop_show;
+}
+function InnerData2List(info_lop) {//add codeclass to MyCodeClassList and Inner Data to site
   //innerHTML ra list danh sách
   // danhsach_selected: List
   // info_danhsach_selected: Info for List
   //<a class="list-group-item list-group-item-action" data-bs-toggle="list" href="#list-home" role="tab">Demo1</a>
   //<div class="tab-pane fade" id="list-home" role="tabpanel">Demo1</br>Demo1</br>Demo1</br>Demo1</div>
+  MyCodeClassList.push(info_lop.MaLop);
   var id = info_lop.MaLop;
   id = id.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");//vì dùng để tạo id nên phải xóa hết các kí tự đặc biệt "."...
   //console.log(id)
   danhsach_selected.innerHTML += 
   `<a class="list-group-item-success mt-1" data-bs-toggle="list" href="#${id}" role="tab" style="text-decoration: none;border-style: solid;">${info_lop.TenMH}</a>`;
   info_danhsach_selected.innerHTML +=
-  `<div class="tab-pane fade" id="${id}" role="tabpanel">${info_lop.TenMH}</br>${info_lop.MaLop}</br>${info_lop.Thu}</br>${info_lop.Tiet}</br>${info_lop.TenGV}</div>`;
+  `<div class="tab-pane fade" id="${id}" role="tabpanel">
+${info_lop.TenMH}</br>${info_lop.MaLop}</br>${info_lop.Thu}</br>${info_lop.Tiet}</br>${info_lop.TenGV}</br>
+<button type="button" class="btn btn-danger" onclick="DeleteMonHoc('${info_lop.MaLop}')">Bỏ chọn học này</button>
+</div>`;
+  handle_show_list_malop();
+  //handle_show_TongTC
+  if(info_lop.SoTc !== undefined) TongTc += parseInt(info_lop.SoTc);
+  show_TongTC.innerHTML = TongTc;
 }
-function OutnerData2List(malop) {
-  var id = malop;
+function OutnerData2List(info_lop) {//remove codeclass to MyCodeClassList and Data to site
+  MyCodeClassList = MyCodeClassList.filter(item => item !== info_lop.MaLop);//remove
+  var id = info_lop.MaLop;
   id = id.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");//vì dùng để tạo id nên phải xóa hết các kí tự đặc biệt "."...
   document.getElementById(id).remove();//remove item khỏi info-danhsach-selected
   //https://stackoverflow.com/questions/10572735/javascript-getelement-by-href
   var i_danhsach_selected = document.querySelectorAll(`a[href='#${id}']`);//remove item khỏi danhsach-selected
-  i_danhsach_selected[0].remove();
+  i_danhsach_selected[0].remove();//mảng này thì chắc chắn chỉ 1pt duy nhất vì href được tạo từ id mà :>
+  handle_show_list_malop();
+  //handle_show_TongTC
+  if(info_lop.SoTc !== undefined) TongTc -= parseInt(info_lop.SoTc);
+  show_TongTC.innerHTML = TongTc;
 }
-
+function DeleteMonHoc(malop) {
+  //https://stackoverflow.com/questions/6267816/getting-element-by-a-custom-attribute-using-javascript
+  var checkBoxChon =  document.querySelector(`input[value-malop="${malop}"]`);
+  var info_lop = GetInfoClassByMaLop(malop);
+  checkBoxChon.checked = false;
+  OutnerData2List(info_lop);
+}
 function CheckAndAddClass2ListChon(checkboxChon) {
   var malop = checkboxChon.getAttribute('value-malop');
+  var info_lop = GetInfoClassByMaLop(malop);
   if (checkboxChon.checked) {
-    var info_lop = GetInfoClassByMaLop(malop);
-    //FIXME: check trùng rồi mới add vào MyCodeClassList[]
-    MyCodeClassList.push(malop);
+    //FIXME: check trùng rồi mới add vào InnerData2List MyCodeClassList[]
     InnerData2List(info_lop);
   }else{
-    OutnerData2List(malop);
-    MyCodeClassList = MyCodeClassList.filter(item => item !== malop);
+    OutnerData2List(info_lop);
   }
 }
-
+//---------------------------------------------------------------------------------------------------------------
 async function Start() {
   //TODO:tạm ẩn để đợi xử lý xong dữ liệu
   start_data.style.display = "";
