8d45469667438a1a4de823bc3019c3ceca3c85e5
hidang <yellowstudio.vn@gmail.com>
2020-12-20 13:39:53 +0700

    byhidang

--- public/soantkbhp.js
+++ public/soantkbhp.js
@@ -1,17 +1,15 @@
-{
-  const back_to_top = document.getElementById('back-to-top');
-  scrollFunction();
-  back_to_top.addEventListener('click', function() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  });
-  window.onscroll = function() {scrollFunction()};
-  function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      back_to_top.style.display = "block";
-    } else {
-      back_to_top.style.display = "none";
-    }
+const back_to_top = document.getElementById('back-to-top');
+scrollFunction();
+back_to_top.addEventListener('click', function() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    back_to_top.style.display = "block";
+  } else {
+    back_to_top.style.display = "none";
   }
 }
 document.getElementById('btnCopy').addEventListener('click', ButtonCopy);
