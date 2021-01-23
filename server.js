78249086e2625d06a9e4f706dfc8ce15574c4d29
hidang <yellowstudio.vn@gmail.com>
2021-01-23 10:03:38 +0700

    byhidang

--- server.js
+++ server.js
@@ -4,21 +4,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(express.json({ limit: '300kb' }));// body-parser defaults to a body size limit of 100kb
+app.use(express.json({ limit: '400kb' }));// body-parser defaults to a body size limit of 400kb
 app.use(bodyParser.json(), express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
 const server = require("http").Server(app);
 server.listen(process.env.PORT || 3000, () =>
-  console.log("Server khởi động thành công!")
+  console.log("Server khởi động thành công! PORT:", process.env.PORT || 3000)
 );
 
-const indexRouter = require('./sources/routes/index');
-const webhook = require('./sources/routes/webhook');
-app.use('/', indexRouter);
-app.use('/webhook', webhook);
-
+app.use('/webhook', require('./sources/routes/webhook'));
+app.use('/', require('./sources/routes/index'));
 
 const helmet = require('helmet');
 app.use(helmet());
